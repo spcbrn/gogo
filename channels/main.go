ac7dbe4f85c1d6b116b30cfcee4d94b98bac35de
spcbrn <spcbrn1@gmail.com>
2019-10-25 17:09:54 -0600

    go routines

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,14 +13,44 @@ func main() {
 		"https://youtube.com",
 		"https://facebook.com",
 		"https://golang.org",
+		"https://christopherlemke.com",
+		"https://suralink.com",
+		"https://udemy.com",
 	}
 
+	c := make(chan string)
+
 	for _, url := range sites {
-		_, err := http.Get(url)
-		if err != nil {
-			fmt.Println(url, "may be down!")
-			return
-		}
-		fmt.Println(url, "appears to be up.")
+		fmt.Println("checking", url)
+		go checkLink(url, c, true)
 	}
+
+	for l := range c {
+		fmt.Println("checking", l)
+		go checkLink(l, c, true)
+		// go func() {
+		// 	time.Sleep(time.Second * 2)
+		// 	checkLink(l, c, false)
+		// }()
+	}
+}
+
+func checkLink(url string, c chan string, loopChan bool) {
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(url, " may be down or unsecured!")
+		if loopChan == true {
+			delayFeedChannel(url, c, 2)
+		}
+		return
+	}
+	fmt.Println(url, " appears to be up.")
+	if loopChan == true {
+		delayFeedChannel(url, c, 2)
+	}
+}
+
+func delayFeedChannel(url string, c chan string, sec time.Duration) {
+	time.Sleep(time.Second * sec)
+	c <- url
 }
